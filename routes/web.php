dec62194e8541077e4b223daee1f1b35b64954a9
ApriCatur <elevenses89@gmail.com>
2026-04-15 12:26:58 +0700

    penambahan controller dan view pada admin

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\login;
-use App\Http\Controllers\databarang;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('login', [login::class, 'index']);
-Route::get('/databarang', [databarang::class, 'tampilkan']);
 
