28574542236fa6e72c4f7e4dae1e7e43ae78951d
ApriCatur <elevenses89@gmail.com>
2026-06-15 13:24:28 +0700

    fix: format jam H:i saja (buang detik)

--- resources/views/panitia/Statistic.blade.php
+++ resources/views/panitia/Statistic.blade.php
@@ -75,7 +75,7 @@
                 </h3>
                 <p class="text-xs text-gray-500 flex items-center gap-2">
                     <i class="fa-solid fa-calendar-day text-blue-500"></i>
-                    {{ \Carbon\Carbon::parse($item->date)->format('d F Y') }}, {{ $item->time_start }} - {{ $item->time_end }} WIB
+                    {{ \Carbon\Carbon::parse($item->date)->format('d F Y') }}, {{ substr($item->time_start, 0, 5) }} - {{ substr($item->time_end, 0, 5) }} WIB
                 </p>
             </div>
 
